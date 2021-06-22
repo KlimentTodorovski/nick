ed65ec3c8efc6a62aac29660acf4fae2e1e0a2c5
Kliment Todorovski <todorovski.kliment1@gmail.com>
2021-06-22 23:43:39 +0200

    Added questions to sports

--- src/app/sport/sport.component.ts
+++ src/app/sport/sport.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ImageType, Question } from '../interfaces/answers';
 
 @Component({
   selector: 'app-sport',
@@ -6,6 +7,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sport.component.css']
 })
 export class SportComponent implements OnInit {
+
+  questions: Question[] = [
+    {
+      question: 'Кој спорт го игра детето на сликата?',
+      options: ['Фудбал', 'Кошарка', 'Одбојка'],
+      answer: 'Фудбал'
+    },
+    {
+      question: 'Кој спорт го игра детето на сликата?',
+      options: ['Ватерполо', 'Кошарка', 'Рагби'],
+      answer: 'Кошарка'
+    },
+    {
+      question: 'Кој спорт го игра детето на сликата?',
+      options: ['Одбојка', 'Ватерполо', 'Тенис'],
+      answer: 'Тенис'
+    },
+    {
+      question: 'Кој спорт го игра детето на сликата?',
+      options: ['Хокеј', 'Тенис', 'Бејзбол'],
+      answer: 'Хокеј'
+    },
+    {
+      question: 'Кој спорт го игра детето на сликата?',
+      options: ['Бејзббол', 'Ракомет', 'Одбојка'],
+      answer: 'Ракомет'
+    },
+    {
+      question: 'Кој спорт го игра детето на сликата?',
+      options: ['Рагби', 'Фудбал', 'Одбојка'],
+      answer: 'Одбојка'
+    },
+    {
+      question: 'Кој спорт го игра детето на сликата?',
+      options: ['Бејзбол', 'Кошарка', 'Ракомет'],
+      answer: 'Бејзбол'
+    },
+    {
+      question: 'Кој спорт го игра детето на сликата?',
+      options: ['Хокеј', 'Ватерполо', 'Тенис'],
+      answer: 'Ватерполо'
+    },
+    {
+      question: 'Кој спорт го игра детето на сликата?',
+      options: ['Одбојка', 'Рагби', 'Фудбал'],
+      answer: 'Рагби'
+    }
+  ];
+
+  imageType: ImageType = ImageType.Sport;
 
   constructor() { }
 
--- src/app/sport/sport.module.ts
+++ src/app/sport/sport.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SportRoutingModule } from './sport-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [ SportRoutingModule.components ],
   imports: [
     CommonModule,
-    SportRoutingModule
+    SportRoutingModule,
+    SharedModule
   ]
 })
 
