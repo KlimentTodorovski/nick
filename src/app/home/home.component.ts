0db0e4f296d568edb09d094fa7d8bdef6427cdeb
elenakaranfilovska <Elena.Karanfilovska@students.finki.ukim.mk>
2021-06-24 22:52:24 +0200

    sound on home page added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,7 +9,17 @@ export class HomeComponent implements OnInit {
 
   constructor() { }
 
+  myAudio;
+
   ngOnInit(): void {
   }
 
+  play(value) {
+    this.myAudio = new Audio(`assets/${value}.mp3`);
+    this.myAudio.play()
+  }
+
+  stop(){
+    this.myAudio.pause()
+  }
 }
