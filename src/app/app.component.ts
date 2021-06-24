c46599bf11ec7045e3a0e30f9f2f44a27a5f4523
Kliment Todorovski <todorovski.kliment1@gmail.com>
2021-06-24 21:25:49 +0200

    make nav toggle working and make math home responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,8 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   title = 'nick';
+  collapsed = true;
+  toggleCollapsed(): void {
+    this.collapsed = !this.collapsed;
+  }
 }
